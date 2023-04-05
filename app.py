55c73783665226c5b79b0f2e0fd35efac935f891
Ethan Gaebel <2490298+egaebel@users.noreply.github.com>
2023-04-05 13:04:29 -0700

    Extract model from g2p pretrained wrapper and set that in banana. Also extracted pre/post processing to be run separately before/after model object is run. Hopefully this will allow banana to fast boot the model.

--- app.py
+++ app.py
@@ -14,24 +14,63 @@ def init():
     device = 0 if torch.cuda.is_available() else -1
     from speechbrain.pretrained import GraphemeToPhoneme
 
-    model = GraphemeToPhoneme.from_hparams(
+    g2p = GraphemeToPhoneme.from_hparams(
         "speechbrain/soundchoice-g2p", run_opts={"device": device}
     )
-    context = {"model": model, "soundchoice_batch_size": 32}
+    context = {"g2p": g2p, "model": g2p.mods.model, "soundchoice_batch_size": 32}
 
     return context
+
+
+def g2p_wrapper_before(g2p, text):
+    """Performs the Grapheme-to-Phoneme conversion
+    Arguments
+    ---------
+    text: str or list[str]
+        a single string to be encoded to phonemes - or a
+        sequence of strings
+    Returns
+    -------
+    result: list
+        if a single example was provided, the return value is a
+        single list of phonemes
+    """
+
+    model_inputs = g2p.encode_input({"txt": text})
+    g2p._update_graphemes(model_inputs)
+    return model_inputs
+
+
+def g2p_wrapper_after(g2p, model_outputs):
+    """Performs the Grapheme-to-Phoneme conversion
+    Arguments
+    ---------
+    text: str or list[str]
+        a single string to be encoded to phonemes - or a
+        sequence of strings
+    Returns
+    -------
+    result: list
+        if a single example was provided, the return value is a
+        single list of phonemes
+    """
+    decoded_output = g2p.decode_output(model_outputs)
+    phonemes = decoded_output["phonemes"]
+    return phonemes
 
 
 # @app.handler runs for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
     text_list = request.json.get("text_list")
+
+    g2p = context.get("g2p")
     model = context.get("model")
     soundchoice_batch_size = context.get("soundchoice_batch_size")
 
     phoneme_list: List[List[str]] = list(
         itertools.chain.from_iterable(
-            model(text_list_chunk)
+            g2p_wrapper_after(g2p, model(**g2p_wrapper_before(g2p, text_list_chunk)))
             for text_list_chunk in more_itertools.chunked(
                 text_list, soundchoice_batch_size
             )
