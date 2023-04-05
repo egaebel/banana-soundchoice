788ab2f5d7c04a2561f8fa7186803f037f048385
Ethan Gaebel <2490298+egaebel@users.noreply.github.com>
2023-04-05 12:26:11 -0700

    Use CPU inference for priming model for download during Docker container creation. Looks like there's no GPU on the build server.

--- download.py
+++ download.py
@@ -1,14 +1,9 @@
-import torch
-
-
 def download_model():
-    device = 0 if torch.cuda.is_available() else -1
     from speechbrain.pretrained import GraphemeToPhoneme
 
-    model = GraphemeToPhoneme.from_hparams(
-        "speechbrain/soundchoice-g2p", run_opts={"device": device}
-    )
-    model("Priming the model to download weights, etc.")
+    model = GraphemeToPhoneme.from_hparams("speechbrain/soundchoice-g2p")
+    result = model("Priming the model to download weights, etc.")
+    print(f"result: '{result}'")
 
 
 if __name__ == "__main__":
