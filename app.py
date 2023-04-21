0dead52888e3bb98eae0da9e1b2b44c72d810012
Ethan Gaebel <2490298+egaebel@users.noreply.github.com>
2023-04-21 10:33:54 -0700

    Add timing to init with logging (now that banana logs are back! )

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import List
 
 import itertools
 import more_itertools
+import time
 import torch
 
 app = Potassium("soundchoice-g2p")
@@ -12,6 +13,7 @@ app = Potassium("soundchoice-g2p")
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
+    init_start_time = int(time.time())
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     print(f"Init with device: '{device}'", flush=True)
     from speechbrain.pretrained import GraphemeToPhoneme
@@ -23,6 +25,11 @@ def init():
         "model": model,
         "soundchoice_batch_size": 32,
     }
+    init_end_time = int(time.time())
+    print(
+        f"Finished running init in: '{init_end_time - init_start_time}' seconds!",
+        flush=True,
+    )
 
     return context
 
